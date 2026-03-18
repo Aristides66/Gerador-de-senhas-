21cdec1cc0dc0c861fc72679e1b5decfd91a2dbf
Aristides <aristides.neto3@aluno.ce.gov.br>
2026-03-18 19:53:32 +0000

    quase pronto

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,32 @@
-const botao = document.getElementById ("botão")
-const input = document.getElementById ("tamanho da senha")
+const botao = document.getElementById("gerar");
 
-botao.addEventListener ("click")
+botao.addEventListener("click", gerarSenha);
+
+function gerarSenha() {
+  const tamanho = document.getElementById("tamanho").value;
+  const maiusculas = document.getElementById("maiusculas").checked;
+  const minusculas = document.getElementById("minusculas").checked;
+  const numeros = document.getElementById("numeros").checked;
+  const simbolos = document.getElementById("simbolos").checked;
+
+  let caracteres = "";
+
+  if (maiusculas) caracteres += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (minusculas) caracteres += "abcdefghijklmnopqrstuvwxyz";
+  if (numeros) caracteres += "0123456789";
+  if (simbolos) caracteres += "!@#$&";
+
+  if (caracteres === "") {
+    alert("Selecione pelo menos uma opção!");
+    return;
+  }
+
+  let senha = "";
+
+  for (let i = 0; i < tamanho; i++) {
+    const random = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres[random]
+  }
+
+  document.getElementById("resultado").textContent = senha
+}
